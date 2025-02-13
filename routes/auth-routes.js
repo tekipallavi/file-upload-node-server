d7bc34dfe46602321f8d2d18e833a4c3cd02de69
tekipallavi <tekipallavi@gmail.com>
2025-02-13 14:39:53 +0530

    Update auth-routes.js

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,1 +1,12 @@
-// This is auth
+const express = require('express');
+const router = express.Router();
+const User = require('../models/UserModel')
+
+router.get('/users', (req, res, next) => {
+   User.find().then( users => {
+       console.log("users from DB", users);
+       res.send(users);
+   })
+});
+
+module.exports = router;
