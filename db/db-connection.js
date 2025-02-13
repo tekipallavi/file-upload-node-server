b5d5271ce247537407aa946260274d7061e989f6
tekipallavi <tekipallavi@gmail.com>
2025-02-13 14:42:33 +0530

    Update db-connection.js

--- db/db-connection.js
+++ db/db-connection.js
@@ -1,6 +1,6 @@
 console.log("in db file")
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://tekipallavi:momsis@cluster0.p8c9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then( result => {
+mongoose.connect('mongodb+srv://tekipallavi:momsis@cluster0.p8c9v.mongodb.net/file-upload?retryWrites=true&w=majority&appName=Cluster0').then( result => {
     console.log('DB connected!!')
 })
