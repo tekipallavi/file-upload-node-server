fd40f28d8aa0af5040df4108678a1b83292ab47a
tekipallavi <tekipallavi@gmail.com>
2025-02-13 15:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('./db/db-connection');
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.use(authRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
