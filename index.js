f746de3bb5e22d64de0cb45873e47940562fb51c
tekipallavi <tekipallavi@gmail.com>
2025-03-13 12:26:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-require('./db/db-connection');
+/* require('./db/db-connection'); */
 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth-routes');
 const fileRoutes = require('./routes/file-routes');
+const mongoose = require("./db/db-connection");
 
 const app = express();
+
+
+mongoose.connection.on("open", () => {    
+console.log("after DB connected lets do file upload", mongoose.connection.db);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(fileRoutes);
+});
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log("start listening")
 })
