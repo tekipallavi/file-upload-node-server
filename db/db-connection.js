c4b3a11367e2e4a8f25b04a1c45ac14c6e5f1a19
tekipallavi <tekipallavi@gmail.com>
2025-03-13 12:06:26 +0530

    Update db-connection.js

--- db/db-connection.js
+++ db/db-connection.js
@@ -4,3 +4,4 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://tekipallavi:momsis@cluster0.p8c9v.mongodb.net/file-upload?retryWrites=true&w=majority&appName=Cluster0').then( result => {
     console.log('DB connected!!')
 })
+module.exports = mongoose;
